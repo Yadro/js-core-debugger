7d41894d0dafd35fdc70fd66c0de3143c6bbcf3f
yadro <yadrone@gmail.com>
2019-07-04 23:44:26 +0300

    Upd tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,16 @@
 import { CoreDebugger } from '../src';
 
 const code = `function func(a, b) {
-\tvar c = 10;
-\tvar d = a + b + c;
+\tvar c = 10, i = 0;
+\ta += b;
+\tvar d = a + c;
 \treturn d;
 }`;
 
 const result = `function func(a, b) {__$YD$__funcParam(1,'a',a);__$YD$__funcParam(1,'b',b);
-\tvar c = 10;__$YD$__varDecl(2,'c',c);
-\tvar d = a + b + c;__$YD$__varDecl(3,'d',d);
+\tvar c = 10, i = 0;__$YD$__varDecl(2,'c',c);__$YD$__varDecl(2,'i',i);
+\ta += b;
+\tvar d = a + c;__$YD$__varDecl(4,'d',d);
 \treturn d;
 }`;
 
